a1ff6bd67c770e1723c05df1d249b14fefe06069
chris <arnold937@yahoo.com>
2021-06-30 22:24:07 +0100

    Jira - 234 Added new Test method

--- GitDemo/src/uk/testing/software/gitdemo/AppClass2.java
+++ GitDemo/src/uk/testing/software/gitdemo/AppClass2.java
@@ -4,4 +4,7 @@ public class AppClass2 {
     public void doSomething(){
         System.out.println("Doing Something from App Class2");
     }
+    public void testUpdate(){
+        System.out.println("Adding another test Method");
+    }
 }
